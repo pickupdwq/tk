f36915184bbc9db58b782c4deb72635c3a6d0381
vic <dwq57@hotmail.com>
2016-09-06 22:55:04 +0800

    new

---
+++ Application/Admin/Controller/ProductController.class.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace Admin\Controller;
+
+/**
+ * 后台首页控制器  
+ * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ */
+class ProductController extends ThinkController {
+
+ 
+}
